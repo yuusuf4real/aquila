ae8332c8ed49f1759036e718b0a6dcdf2b1077c6
yusuf <yuusuf4real@yahoo.com>
2022-04-26 10:26:21 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Main template file
+ * 
+ * @package aquila
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WordPress Theme</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <script src="index.js"></script>
+</body>
+
+</html>
